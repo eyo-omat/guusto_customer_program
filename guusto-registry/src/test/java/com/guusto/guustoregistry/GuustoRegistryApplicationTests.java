dabbadb5fc35e2f575d39560fd31ca5de5272506
Eyioyo Omatsola <eyo.omat@gmail.com>
2022-03-20 23:23:43 +0000

    :clown_face: Added Dockerfile to registry server

---
+++ guusto-registry/src/test/java/com/guusto/guustoregistry/GuustoRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guusto.guustoregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuustoRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
